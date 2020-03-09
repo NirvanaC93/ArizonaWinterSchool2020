87879fc0e11554b8554af91d9ae5e64201ec2dff
Nicolas Mueller <nicolas.mueller@math.ethz.ch>
2020-03-09 03:18:03 +0100

    Added _q files

--- Coleman/coleman.m
+++ Coleman/coleman.m
@@ -1,9 +1,9 @@
 Qx<x>:=PolynomialRing(RationalField());
 Qxy<y>:=PolynomialRing(Qx);
 
-load "auxpolys.m";
-load "coho.m";
-load "froblift.m";
-load "reductions.m";
+load "auxpolys_q.m";
+load "coho_q.m";
+load "froblift_q.m";
+load "reductions_q.m";
 load "singleintegrals.m";
 load "applications.m";

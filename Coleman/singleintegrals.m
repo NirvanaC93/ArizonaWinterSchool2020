6bfe7cfd22f5689c4286911e07494d4dbc88e2ca
Nicolas Mueller <nicolas.mueller@math.ethz.ch>
2020-03-10 05:40:07 +0100

    Made set_point function number field ready

--- Coleman/singleintegrals.m
+++ Coleman/singleintegrals.m
@@ -184,11 +184,13 @@ coleman_data:=function(Q,p,m,N:useU:=false,basis0:=[],basis1:=[],basis2:=[],verb
 
   // formatting the output into a record:
 
-  format:=recformat<Q,p,N,g,W0,Winf,r,Delta,s,G0,Ginf,e0,einf,delta,basis,quo_map,integrals,F,f0list,finflist,fendlist,Nmax,red_list_fin,red_list_inf,minpolys>;
+  format:=recformat<Q,p,N,g,W0,Winf,r,Delta,s,G0,Ginf,e0,einf,delta,basis,quo_map,integrals,F,f0list,finflist,fendlist,Nmax,red_list_fin,red_list_inf,minpolys,K,n>;
   out:=rec<format|>;
   out`Q:=Q; out`p:=p; out`N:=N; out`g:=g; out`W0:=W0; out`Winf:=Winf; out`r:=r; out`Delta:=Delta; out`s:=s; out`G0:=G0; out`Ginf:=Ginf; 
   out`e0:=e0; out`einf:=einf; out`delta:=delta; out`basis:=basis; out`quo_map:=quo_map; out`integrals:=integrals; out`F:=F; out`f0list:=f0list; 
   out`finflist:=finflist; out`fendlist:=fendlist; out`Nmax:=Nmax; out`red_list_fin:=red_list_fin; out`red_list_inf:=red_list_inf;
+  out`K:=K;
+  out`n:=n;
 
   return out;
 
@@ -200,14 +202,15 @@ set_point:=function(x0,y0,data)
   // Constructs a point from affine coordinates x0,y0. 
 
   Q:=data`Q; p:=data`p; N:=data`N; W0:=data`W0;
+  K:=data`K; n:=data`n;
   d:=Degree(Q);
 
-  if x0 in RationalField() then
-    K:=pAdicField(p,N);
+  if x0 in data`K then
+    Kp:=ext<pAdicField(p,N)|n>;
   else
-    K:=Parent(x0);
+    Kp:=Parent(x0);
   end if;
-  x0:=K!x0; y0:=K!y0;
+  x0:=Kp!x0; y0:=Kp!y0;
 
   if Valuation(x0) lt 0 then
     error "x0 has negative valuation";
@@ -223,7 +226,7 @@ set_point:=function(x0,y0,data)
   P`x:=x0;
 
   y0powers:=[];
-  y0powers[1]:=K!1;
+  y0powers[1]:=Kp!1;
   for i:=2 to d do
     y0powers[i]:=(y0)^(i-1);
   end for;
